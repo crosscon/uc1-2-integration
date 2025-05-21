c8ab5272db6ff81ff8773f8de01186907a6b7ecc
Pawel Langowski <pawel.langowski@3mdeb.com>
2025-05-21 11:12:40 +0200

    scripts/buildroot_ta_key_gen.sh: Generate keys for client
    Modified the script to also generate a key pair to be used in the client
    application. This is necessary to perform certificate-based
    authentication during TLS handshake.
    
    Signed-off-by: Pawel Langowski <pawel.langowski@3mdeb.com>

--- scripts/buildroot_ta_key_gen.sh
+++ scripts/buildroot_ta_key_gen.sh
@@ -3,8 +3,11 @@ set -e
 
 source "$(dirname "$0")/common.sh"
 
+# TODO: Use separate RPis for server and client
 echo "# Attempting to generate private keys on Pi via PKCS#11 TA!"
 ssh "$PI" sh -i<<EOF
+# Server
+
 # List slots
 pkcs11-tool --module /usr/lib/libckteec.so -L
 # Init token
@@ -21,7 +24,25 @@ pkcs11-tool --module /usr/lib/libckteec.so \
   --slot 0 --login --pin 1234 \
   --read-object --type pubkey --id 01 \
   --output-file server-pubkey.der
+
+# Client
+
+# Init token
+pkcs11-tool --module /usr/lib/libckteec.so --init-token --slot 1 --label "ClientToken" --so-pin 1234
+# Init pin
+pkcs11-tool --module /usr/lib/libckteec.so --slot 1 --login --so-pin 1234 --init-pin --pin 1234
+# Generate key pair
+pkcs11-tool --module /usr/lib/libckteec.so \
+  --slot 1 --login --pin 1234 \
+  --keypairgen --key-type rsa:2048 \
+  --label "ClientKey" --id 01
+# Export pubkey
+pkcs11-tool --module /usr/lib/libckteec.so \
+  --slot 1 --login --pin 1234 \
+  --read-object --type pubkey --id 01 \
+  --output-file client-pubkey.der
+
 EOF
 mkdir -p ./artifacts/certs/
-scp $PI:~/server-pubkey.der ./artifacts/certs/
-echo "# Keys generated, pubkey fetched!"
+scp $PI:~/server-pubkey.der $PI:~/client-pubkey.der ./artifacts/certs/
+echo "# Keys generated, pubkeys fetched!"
