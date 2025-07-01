67000e30198752fb5bc47dd77482ec3bcd073f30
Mateusz Kusiak <mateusz.kusiak@3mdeb.com>
2025-07-01 15:36:40 +0200

    certs: move legacy certs
    Current solution relies on generating certificates dynamically. Move
    redundant certificates to legacy subdirectory. They might be handy for
    LPC55s69 soultion integration.
    
    Signed-off-by: Mateusz Kusiak <mateusz.kusiak@3mdeb.com>

--- scripts/buildroot_ta_cert_gen.sh
+++ scripts/buildroot_ta_cert_gen.sh
@@ -4,75 +4,40 @@ set -e
 
 source "$(dirname "$0")/common.sh"
 
+scp ./certs/cert.conf $PI_SERVER:~
+scp ./certs/cert.conf $PI_CLIENT:~
+
 DATE_STRING=$(date +"%m%d%H%M%Y")
 
-
-print_usage() {
-  echo "$0: Generate certificates out of CSR's produced on RPis. This script needs"
-  echo "variables PI_SERVER and PI_CLIENT to be set accordingly. Check README.md"
-  echo "and scripts/common.sh for more information."
-  echo "Usage:"
-  echo "  $0: generate certificates for both: client and server"
-  echo "  $0 --server: generate certificate for server only"
-  echo "  $0 --client: generate certificate for client only"
-
-  exit 0
-}
-
-# Arguments:
-# 1: target for SSH in form "user@ip"
-# 2: file name prefix for the CSR, e.g. "client"
-# 3: PKCS#11 token name
-# 4: PKCS#11 object in the token
-generate_cert() {
-  local target, prefix, token, object
-  target=$1
-  prefix=$2
-  token=$3
-  object=$4
-
-  scp ./certs/cert.conf $target:~
-
-  echo "Generating CSR for target $target..."
-  ssh "$target" sh -i<<EOF
+echo "# Generating server CSR"
+ssh "$PI_SERVER" sh -i<<EOF
 export PKCS11_MODULE_PATH=/usr/lib/libckteec.so
 openssl req -engine pkcs11 -keyform engine \
-  -key "pkcs11:token=$token;object=$object;type=private;pin-value=1234" \
-  -new -config ~/cert.conf -out ~/$prefix-csr.pem
+  -key "pkcs11:token=ServerToken;object=ServerKey;type=private;pin-value=1234" \
+  -new -config ~/cert.conf -out ~/server-csr.pem 
 date $DATE_STRING
 EOF
-  echo "Done!"
 
-  echo "Copying the CSR to host..."
-  scp $target:~/$prefix-csr.pem ./artifacts/certs
-  echo "Done!"
+ssh "$PI_CLIENT" sh -i<<EOF
+export PKCS11_MODULE_PATH=/usr/lib/libckteec.so
+openssl req -engine pkcs11 -keyform engine \
+  -key "pkcs11:token=ClientToken;object=ClientKey;type=private;pin-value=1234" \
+  -new -config ~/cert.conf -out ~/client-csr.pem 
+date $DATE_STRING
+EOF
 
-  echo "Generating cerificate for target: $target..."
-  openssl pkey -pubin -inform DER -in ./artifacts/certs/$prefix-pubkey.der -out ./artifacts/certs/$prefix-pubkey.pem
-  openssl x509 -req -days 365 -in ./artifacts/certs/$prefix-csr.pem -CA ./certs/ca-cert.pem -CAkey ./certs/ca-key.pem -CAcreateserial -out ./artifacts/certs/$prefix-cert.pem -extfile ./certs/cert.conf -extensions req_ext
-  echo "Done!"
+scp $PI_SERVER:~/server-csr.pem ./artifacts/certs
+scp $PI_CLIENT:~/client-csr.pem ./artifacts/certs
 
-  echo "Copying the CA and target certificate to target $target..."
-  scp ./artifacts/certs/$prefix-cert.pem "$target":~
-  scp ./certs/ca-cert.pem "$target":~
-  echo "Done!"
-}
+echo "# Generating server certificate"
+openssl pkey -pubin -inform DER -in ./artifacts/certs/server-pubkey.der -out ./artifacts/certs/server-pubkey.pem
+openssl x509 -req -days 365 -in ./artifacts/certs/server-csr.pem -CA ./certs/ca-cert.pem -CAkey ./certs/ca-key.pem -CAcreateserial -out ./artifacts/certs/server-cert.pem -extfile ./certs/cert.conf -extensions req_ext
 
-# Handle params
-for arg in "$@"; do
-  case "$arg" in
-    --host)
-      echo "Generating certificates for server under IP: $PI_SERVER_HOST"
-      generate_cert "$PI_SERVER_HOST" "server" "ServerToken" "ServerKey"
-      ;;
-    --client)
-      echo "Generating certificates for client under IP: $PI_CLIENT_HOST"
-      generate_cert "$PI_CLIENT_HOST" "client" "ClientToken" "ClientKey"
-      ;;
-    *)
-      echo "Generating certificates for client under IP: $PI_CLIENT_HOST and for host under IP: $PI_SERVER_HOST"
-      generate_cert "$PI_CLIENT_HOST" "client" "ClientToken" "ClientKey"
-      generate_cert "$PI_SERVER_HOST" "server" "ServerToken" "ServerKey"
-      ;;
-  esac
-done
+echo "# Generating client cerificate"
+openssl pkey -pubin -inform DER -in ./artifacts/certs/client-pubkey.der -out ./artifacts/certs/client-pubkey.pem
+openssl x509 -req -days 365 -in ./artifacts/certs/client-csr.pem -CA ./certs/ca-cert.pem -CAkey ./certs/ca-key.pem -CAcreateserial -out ./artifacts/certs/client-cert.pem -extfile ./certs/cert.conf -extensions req_ext
+
+scp ./artifacts/certs/server-cert.pem "$PI_SERVER":~ 
+scp ./artifacts/certs/client-cert.pem "$PI_CLIENT":~
+scp ./certs/ca-cert.pem "$PI_SERVER":~
+scp ./certs/ca-cert.pem "$PI_CLIENT":~
